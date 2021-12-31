80be85b342a04e9e3db1b8a1ccaa0c1a48879d15
Roshita <roshitha.23it@licet.ac.in>
2021-12-31 20:53:02 +0530

    add configs

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
-String backendURL = "https://059b-27-5-243-120.ngrok.io";
+String backendURL = "https://c3f6-27-5-153-135.ngrok.io";
 
-String deviceId = "9WceR6d9BgLnRYANAAAJ";
+String deviceId = "4rBRnoBzsffg_dElAAAB";
